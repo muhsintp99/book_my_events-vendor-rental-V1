8006f9e3d3f184f3652602d8d082f2910da3d5c5
Sreerag o s <sreeragsnair142@gmail.com>
2026-01-10 19:50:14 +0530

    dist vendor

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8kj3on51gmg"
+    "revision": "0.rhnib89jtbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
