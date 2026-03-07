77b77c7756a5096c734cc1d9ad62996a57ee1d8b
Sreerag o s <sreeragsnair142@gmail.com>
2026-03-07 12:39:38 +0530

    hide add package1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.57iv094eqqg"
+    "revision": "0.jgh6fgp10po"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
