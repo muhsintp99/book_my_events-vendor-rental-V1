1957fff2ca8f7997201bc83c478c3a00bb181bf1
Sreerag o s <sreeragsnair142@gmail.com>
2026-03-18 22:37:18 +0530

    update dashboard all

---
+++ dist/assets/Categories-B4CHDcl4.js
@@ -0,0 +1,1 @@
+import"./index-D6uvY-CC.js";import"./jspdf.plugin.autotable-DTipuz1k.js";const a=OrnamentsCategory;export{a as default};
--- dist/assets/Categories-DVjpcFGv.js
+++
@@ -1,1 +0,0 @@
-import"./index-GWooxGaJ.js";import"./jspdf.plugin.autotable-QgoJNn3T.js";const a=OrnamentsCategory;export{a as default};
